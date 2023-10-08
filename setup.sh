3d10a6072f83545a169c802ab41afae888d8ece2
Rafael Bulsing <rafabulsing@gmail.com>
2023-10-08 17:37:25 -0300

    Comment out parts to test syncthing setup

--- setup.sh
+++ setup.sh
@@ -17,38 +17,38 @@ sudo systemctl enable syncthing@$USER
 sudo systemctl start syncthing@$USER
 
 # install CUDA (from https://developer.nvidia.com/cuda-downloads)
-echo 'Installing CUDA...'
-wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.run
-sudo sh cuda_12.2.2_535.104.05_linux.run --silent
+# echo 'Installing CUDA...'
+# wget https://developer.download.nvidia.com/compute/cuda/12.2.2/local_installers/cuda_12.2.2_535.104.05_linux.run
+# sudo sh cuda_12.2.2_535.104.05_linux.run --silent
 
-# install git-lfs
-echo 'Installing git-lfs...'
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
-sudo -u ubuntu git lfs install --skip-smudge
+# # install git-lfs
+# echo 'Installing git-lfs...'
+# curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+# sudo apt-get install git-lfs
+# sudo -u ubuntu git lfs install --skip-smudge
 
-# clone SD repo
-echo 'Cloning Stable Diffusion Web UI repository...'
-sudo -u ubuntu git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
+# # clone SD repo
+# echo 'Cloning Stable Diffusion Web UI repository...'
+# sudo -u ubuntu git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
 
-# download the SD model v2.1 and move it to the SD model directory
-echo 'Downloading SD model...'
-sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
-cd stable-diffusion-2-1-base/
-sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
-sudo -u ubuntu git lfs install --force
-cd ..
-mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
-rm -rf stable-diffusion-2-1-base/
+# # download the SD model v2.1 and move it to the SD model directory
+# echo 'Downloading SD model...'
+# sudo -u ubuntu git clone --depth 1 https://huggingface.co/stabilityai/stable-diffusion-2-1-base
+# cd stable-diffusion-2-1-base/
+# sudo -u ubuntu git lfs pull --include "v2-1_512-ema-pruned.ckpt"
+# sudo -u ubuntu git lfs install --force
+# cd ..
+# mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/models/Stable-diffusion/
+# rm -rf stable-diffusion-2-1-base/
 
-# download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
-echo 'Downloading model configuration file...'
-wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
-cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
+# # download the corresponding config file and move it also to the model directory (make sure the name matches the model name)
+# echo 'Downloading model configuration file...'
+# wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
+# cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
 
-# change ownership of the web UI so that a regular user can start the server
-sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
+# # change ownership of the web UI so that a regular user can start the server
+# sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
 
-# start the server as user 'ubuntu'
-echo 'Starting web UI...'
-sudo -u ubuntu nohup bash stable-diffusion-webui/webui.sh --xformers
+# # start the server as user 'ubuntu'
+# echo 'Starting web UI...'
+# sudo -u ubuntu nohup bash stable-diffusion-webui/webui.sh --xformers
